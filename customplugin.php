d4887d5a12565447d8dd79a3320d7cb197992335
MAKHFI YOUNESS <younessdeveloper@gmail.com>
2020-06-29 18:01:17 +0100

    add plugin icon

--- customplugin.php
+++ customplugin.php
@@ -41,7 +41,7 @@ register_deactivation_hook( __FILE__, 'delete_people_table' );
 // Add menu
 function customplugin_menu() {
 
-    add_menu_page("Custom Plugin", "Custom Plugin","manage_options", "myplugin", "displayList",plugins_url('/customplugin/img/icon.png'));
+    add_menu_page("Custom Plugin", "Custom Plugin","manage_options", "myplugin", "displayList",'dashicons-chart-area');
     add_submenu_page("myplugin","All Entries", "All entries","manage_options", "allentries", "displayList");
     add_submenu_page("myplugin","Add new Entry", "Add new Entry","manage_options", "addnewentry", "addEntry");
 
